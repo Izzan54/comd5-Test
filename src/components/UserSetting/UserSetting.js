d4e9fe1f9f12816d91f24137faddaa75049064e3
Nadiah Zairee <nadizeq@yahoo.com>
2022-03-07 08:13:42 +0000

    user-setting

--- src/components/UserSetting/UserSetting.js
+++ src/components/UserSetting/UserSetting.js
@@ -37,19 +37,35 @@ export const UserSetting = () => {
   return (
     <div>
       {/* <div className="w-full h-screen"> */}
-      <div className="flex flex-col items-center justify-center pt-4 mt-2 ml-2 xl:items-start xl:mt-10 xl:gap-20 xl:flex-row ">
-        <div className="mb-4 xl:mb-0 rounded-lg shadow-xl box w-11/12 xl:w-auto border-[#376db3] border-8">
+      <div
+        className="flex flex-col items-center justify-center
+       pt-4 mt-2 ml-2 
+
+       lg:mx-10
+       lg:items-start 
+        lg:gap-10 lg:flex-row 
+
+       xl:items-start xl:mt-10 xl:gap-20 xl:flex-row "
+      >
+        <div
+          className="mb-4 xl:mb-0 rounded-lg shadow-xl box w-11/12
+        lg: xl:w-auto border-[#376db3] border-8"
+        >
           <div className="mx-3 my-5 sm:mx-20 sm:my-10 ">
-            <p className="mb-3 text-xl font-bold text-white sm:text-3xl">
+            <p className="mb-3 lg:text-2xl text-xl font-bold text-white sm:text-3xl">
               Personal Information
             </p>
-            <div className="flex flex-wrap justify-center w-full">
+            <div className="flex flex-wrap justify-center w-full ">
               <img
                 src="https://www.creative-tim.com/learning-lab/tailwind-starter-kit/img/team-1-800x800.jpg"
                 alt="..."
-                className="h-32 border-none rounded-full shadow xs:h-44"
+                className="h-32 border-none rounded-full shadow xs:h-44
+                lg:h-28"
               />
-              <p className="mt-8 ml-2 text-xl font-bold text-white xxs:text-2xl xxs:mt-8 xxs:ml-6 xs:text-5xl xs:mt-10 xs:ml-10 ">
+              <p
+                className="mt-8 ml-2 text-xl font-bold text-white 
+              lg:text-xl xxs:text-2xl xxs:mt-8 xxs:ml-6 xs:text-5xl xs:mt-10 xs:ml-10 "
+              >
                 {firstname} <br />
                 {lastName}
               </p>
@@ -109,10 +125,17 @@ export const UserSetting = () => {
             </div>
           </div>
         </div>
-        <div className="col-span-1 mb-5 rounded-lg shadow-xl box w-11/12 lg:mb-0 2xl:mb-8 lg:pb-1 border-[#376db3] border-8 xl:max-w-xl  xl:w-full">
+        <div
+          className="col-span-1 mb-5 rounded-lg shadow-xl box w-11/12
+          
+        md:my-3 lg:mb-0 2xl:mb-8 lg:pb-1 border-[#376db3] border-8 xl:max-w-xl  xl:w-full"
+        >
           {" "}
           <div className="h-full mx-3 my-5 sm:mx-20 sm:my-10">
-            <p className="mb-3 text-xl font-bold text-white sm:text-2xl">
+            <p
+              className="mb-3 text-xl
+            lg:text-2xl  font-bold text-white sm:text-2xl"
+            >
               Email & Password
             </p>
             <div className="mb-3">
